4fce8a69280b9744af69492f4859db743183ec4a
github-ci <you@example.com>
2025-05-05 03:49:45 +0000

    refactor: 移除旧的CRUD控制器并添加属性控制器加载器

---
+++ src/Service/AttributeControllerLoader.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TelegramBotBundle\Service;
+
+use Symfony\Bundle\FrameworkBundle\Routing\AttributeRouteControllerLoader;
+use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+use Symfony\Component\Routing\RouteCollection;
+use TelegramBotBundle\Controller\WebhookController;
+use Tourze\RoutingAutoLoaderBundle\Service\RoutingAutoLoaderInterface;
+
+#[AutoconfigureTag('routing.loader')]
+class AttributeControllerLoader extends Loader implements RoutingAutoLoaderInterface
+{
+    private AttributeRouteControllerLoader $controllerLoader;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->controllerLoader = new AttributeRouteControllerLoader();
+    }
+
+    public function load(mixed $resource, ?string $type = null): RouteCollection
+    {
+        return $this->autoload();
+    }
+
+    public function supports(mixed $resource, ?string $type = null): bool
+    {
+        return false;
+    }
+
+    public function autoload(): RouteCollection
+    {
+        return $this->controllerLoader->load(WebhookController::class);
+    }
+}
