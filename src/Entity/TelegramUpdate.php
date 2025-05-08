3942deec72abf7cca74e926394e02d53586f3d79
github-ci <you@example.com>
2025-05-08 08:46:33 +0000

    fix

--- src/Entity/TelegramUpdate.php
+++ src/Entity/TelegramUpdate.php
@@ -22,7 +22,6 @@ class TelegramUpdate implements PlainArrayInterface
 {
     #[ExportColumn]
     #[ListColumn(order: -1, sorter: true)]
-    #[Groups(['restful_read', 'admin_curd', 'recursive_view', 'api_tree'])]
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
