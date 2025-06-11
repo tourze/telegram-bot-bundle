bb3a95e4f3ef2b2b0ca182c305fc4337a016c5a4
github-ci <you@example.com>
2025-06-12 06:51:31 +0800

    switch

--- src/TelegramBotBundle.php
+++ src/TelegramBotBundle.php
@@ -4,13 +4,14 @@ namespace TelegramBotBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\DoctrineAsyncInsertBundle\DoctrineAsyncInsertBundle;
 
 class TelegramBotBundle extends Bundle implements BundleDependencyInterface
 {
     public static function getBundleDependencies(): array
     {
         return [
-            \Tourze\DoctrineAsyncBundle\DoctrineAsyncBundle::class => ['all' => true],
+            DoctrineAsyncInsertBundle::class => ['all' => true],
         ];
     }
 }
