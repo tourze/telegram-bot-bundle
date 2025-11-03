61b2f81c97b240204ba60a64f3ead5195eb484e5
github-ci <you@example.com>
2025-11-03 14:24:29 +0800

    update++++++++++++

--- src/TelegramBotBundle.php
+++ src/TelegramBotBundle.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\DoctrineAsyncInsertBundle\DoctrineAsyncInsertBundle;
 use Tourze\RoutingAutoLoaderBundle\RoutingAutoLoaderBundle;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class TelegramBotBundle extends Bundle implements BundleDependencyInterface
 {
@@ -16,6 +17,7 @@ class TelegramBotBundle extends Bundle implements BundleDependencyInterface
             DoctrineBundle::class => ['all' => true],
             DoctrineAsyncInsertBundle::class => ['all' => true],
             RoutingAutoLoaderBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
